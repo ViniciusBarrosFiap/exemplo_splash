888d18e735a22cc119851660f5458c504576de6b
Vinicius Barros <126582852+ViniciusBarrosFiap@users.noreply.github.com>
2024-08-12 10:50:52 -0300

    Add files via upload
    first commit

---
+++ exemplo_splash/src/main/java/com/fiap/exemplo_splash/Exemplo_splash.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fiap.exemplo_splash;
+
+import com.fiap.View.Splash_GUI;
+
+/**
+ *
+ * @author labsfiap
+ */
+public class Exemplo_splash {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Splash_GUI().setVisible(true);
+    }
+}
